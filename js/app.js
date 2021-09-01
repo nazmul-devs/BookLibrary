b0dae336cdc103423095ea9fb296f1b1f3036130
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-09-02 02:10:48 +0600

    simple part completed

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ const bookContainer = document.getElementById('book-container');
 
 
 // loader
-const loaderStyle = style => {
+const loadingToggle = style => {
     document.getElementById('loader').style.display = style;
 }
-const bookToggle = style => {
+const mainContainerToggle = style => {
     document.getElementById('results-details').style.display = style;
 }
 
@@ -16,25 +16,43 @@ const bookToggle = style => {
 const searchValue = () => {
     loadResuls(searchInput.value); // call loadResults function
     searchInput.value = '';
+    bookContainer.textContent = '';
     // toggle loading
-    bookToggle('none');
-    loaderStyle('block');
+    mainContainerToggle('none');
+    loadingToggle('block');
 };
 
 // get API result 
 const loadResuls = async input => {
     const res = await fetch(`http://openlibrary.org/search.json?q=${input}`);
     const data = await res.json();
+    if (data.numFound === 0) {
+        foundResult.innerText = "No result's found";
+        loadingToggle('none');
+        mainContainerToggle('block');
+        return
+    }
     displaData(data);
 };
 // display data to UI
 const displaData = books => {
     foundResult.innerText = `Found result ${books.numFound}`;
-    books.docs.forEach(book => {
+    const showBooks = books.docs.slice(0, 35);
+    showBooks?.forEach(book => {
+        // get image
+        let imgUrl = "";
+        if (book.cover_i === undefined) {
+            imgUrl = "img/book-cover-1.jpg";
+        } else {
+            imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
+        }
+          
+        // create div
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
+            <img src="${imgUrl}" alt="">
             <div class="card-body">
                 <h5 class="card-title fw-bold">${book.title}</h5>
                 <h5 class="fs-5">Author: ${book.author_name}</h5>
@@ -47,6 +65,18 @@ const displaData = books => {
     })
     
     // toggle loading
-    bookToggle('block');
-    loaderStyle('none');
+    mainContainerToggle('block');
+    loadingToggle('none');
+}
+
+// get image
+const getImg = id => {
+    if (id === undefined) {
+        console.log('hwllo');
+    } else {
+        fetch()
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }
+    
 }
